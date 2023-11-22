a54afe430598888627b6040368f02635dff8bf25
Matthew222584 <114376634+Matthew222584@users.noreply.github.com>
2023-11-21 22:16:36 -0500

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menu = document.querySelector(".navigationbar");
+    const grid = document.querySelector(".hamburger .grid-list");
+
+    menu.addEventListener("click", function () {
+        grid.classList.toggle("show-dropdown");
+    });
+});
